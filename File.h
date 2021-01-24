e748c1cf74344335f654572e80b235e7265a19f2
alikhan1998 <77904698+alikhan1998@users.noreply.github.com>
2021-01-24 16:32:59 -0500

    Update File.h

--- File.h
+++ File.h
@@ -1,18 +1,21 @@
-#ifndef   SDDS_FILE_H__
-#define   SDDS_FILE_H__
+#ifndef   SDDS_FILE_H
+#define   SDDS_FILE_H
  #include "ShoppingRec.h"
  #include <iostream>
 
-namespace sdds{
+const char* const SHOPPING_DATA_FILE = "shoppinglist.csv";
 
+namespace sdds{
+ 
+ FILE* sfptr = nullptr;
 bool openFileForRead();
 bool openFileForOverwrite();
 void closeFile();
 bool freadShoppingRec(ShoppingRec* rec);
 void fwriteShoppintRec(const ShoppingRec* rec);
 
-const char* const SHOPPING_DATA_FILE = "shoppinglist.csv";
-FILE* sfptr = nullptr;
+
+
 
 
 }

16d65a6ad0c37614556a414e77c57cd7c9ebf9ed
alikhan1998 <77904698+alikhan1998@users.noreply.github.com>
2021-01-24 16:34:00 -0500

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -1,10 +1,11 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef SDDS_UTILS_H
+#define SDDS_UTILS_H
 
+namespace sdds{
 void flushkeys();
 bool ValidYesResponse(char ch);
 bool yes();
 void readCstr(char cstr[], int len);
 int readInt(int min, int max);
-
+}
 #endif
